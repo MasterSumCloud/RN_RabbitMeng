af1ddcf1eb7c74c07ae7bb422f6d31ec3908813a
Mac_py <summer.p.y@foxmail.com>
2018-06-28 18:29:29 +0800

    我的页面

--- RabbitMeng/js/UI/MineUI.js
+++ RabbitMeng/js/UI/MineUI.js
@@ -3,9 +3,11 @@ import {
     StyleSheet,
     View,
     Image,
-    Text
+    Text,
+    ImageBackground
 } from 'react-native';
 
+let ScreenUtil = require('../uitl/ScreenUtil');
 
 export default class MineUI extends Component {
 
@@ -20,7 +22,28 @@ export default class MineUI extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>我的</Text>
+                <ImageBackground resizeMode='contain' style={styles.mine_top}
+                                 source={require('../../res/imgs/mine_top_bg.jpg')}
+                >
+                    {/*头像*/}
+                    <Image style={styles.image_login_head} roundAsCircle={true}
+                           source={require('../../res/imgs/head_defoult.jpg')}/>
+
+                    <Text>
+                        认证
+                    </Text>
+                </ImageBackground>
+                /*中间按钮*/
+                <View>
+
+                </View>
+
+                /*底部List*/
+                <View>
+
+                </View>
+
+                </View>
             </View>
         );
     }
@@ -31,6 +54,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#FFFFFF',
+    },
+    mine_top: {
+        width: ScreenUtil.screenW,
+        height: ScreenUtil.scaleSize(500),
+        justifyContent: 'center',
     }
 });
