4cbcdbde351d45d1d751fa7afb2eee0d90c81995
Py-Master <summer.p.y@foxmail.com>
2018-07-02 00:04:19 +0800

    调整

--- RabbitMeng/js/UI/MineUI.js
+++ RabbitMeng/js/UI/MineUI.js
@@ -4,7 +4,8 @@ import {
     View,
     Image,
     Text,
-    ImageBackground
+    ImageBackground,
+    ScrollView
 } from 'react-native';
 
 let ScreenUtil = require('../uitl/ScreenUtil');
@@ -22,27 +23,34 @@ export default class MineUI extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <ImageBackground resizeMode='contain' style={styles.mine_top}
-                                 source={require('../../res/imgs/mine_top_bg.jpg')}
+                <ScrollView
+                    style={{flex: 1, flexDirection: 'column'}}
                 >
-                    {/*头像*/}
-                    <Image style={styles.image_login_head} roundAsCircle={true}
-                           source={require('../../res/imgs/head_defoult.jpg')}/>
+                    <ImageBackground resizeMode='contain' style={styles.mine_top}
+                                     source={require('../../res/imgs/mine_top_bg.jpg')}
+                    >
+                        {/*头像*/}
+                        <Image style={styles.image_login_head} roundAsCircle={true}
+                               source={require('../../res/imgs/head_defoult.jpg')}/>
 
-                    <Text style={styles.text_white}>
-                        天使的守护
-                    </Text>
-                </ImageBackground>
-                <View>
+                        <Text style={styles.text_white}>
+                            天使的守护
+                        </Text>
 
+                        <View style={styles.identify_bg}>
+                            <Text style={{color: 'white'}}>认证</Text>
+                        </View>
+                    </ImageBackground>
                     /*中间按钮*/
-                </View>
+                    <View style={styles.top_importent}>
 
-                <View>
+                    </View>
 
                     /*底部List*/
-                </View>
+                    <View style={styles.bottom_list}>
 
+                    </View>
+                </ScrollView>
             </View>
         );
     }
@@ -51,21 +59,51 @@ export default class MineUI extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FFFFFF',
+        backgroundColor: '#EDF1F4',
     },
     mine_top: {
-        alignItems:'center',
+        alignItems: 'center',
         width: ScreenUtil.screenW,
         height: ScreenUtil.scaleSize(500),
     },
     image_login_head: {
-        marginTop:ScreenUtil.scaleSize(100),
+        marginTop: ScreenUtil.scaleSize(100),
         width: ScreenUtil.scaleSize(140),
         height: ScreenUtil.scaleSize(140),
         borderRadius: 35,
     },
-    text_white:{
-        marginTop:ScreenUtil.scaleSize(15),
-        color:'white'
+    text_white: {
+        marginTop: ScreenUtil.scaleSize(15),
+        color: 'white'
+    },
+    identify_bg: {
+        paddingTop: ScreenUtil.scaleSize(5),
+        paddingBottom: ScreenUtil.scaleSize(5),
+        paddingLeft: ScreenUtil.scaleSize(15),
+        paddingRight: ScreenUtil.scaleSize(15),
+        marginTop: ScreenUtil.scaleSize(15),
+        borderWidth: ScreenUtil.scaleSize(1),
+        borderColor: 'white',
+        backgroundColor: 'rgba(255,255,255,0.6)',
+        borderRadius: ScreenUtil.scaleSize(25)
+    },
+    top_importent: {
+        marginLeft: ScreenUtil.scaleSize(30),
+        marginRight: ScreenUtil.scaleSize(30),
+        height: ScreenUtil.scaleSize(150),
+        width: ScreenUtil.screenW - ScreenUtil.scaleSize(60),
+        backgroundColor: 'white',
+        borderRadius: ScreenUtil.scaleSize(30),
+        position: 'absolute',
+        marginTop: ScreenUtil.scaleSize(425),
+    },
+    bottom_list: {
+        height: ScreenUtil.scaleSize(450),
+        marginTop: ScreenUtil.scaleSize(110),
+        marginLeft: ScreenUtil.scaleSize(30),
+        marginRight: ScreenUtil.scaleSize(30),
+        width: ScreenUtil.screenW - ScreenUtil.scaleSize(60),
+        backgroundColor: 'white',
+        borderRadius: ScreenUtil.scaleSize(30),
     }
 });
