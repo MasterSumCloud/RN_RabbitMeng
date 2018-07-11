308c05952319d3eebbb2530165fd1f11d3f31d2b
Mac_py <summer.p.y@foxmail.com>
2018-07-11 16:47:43 +0800

    部队UI图片数据整理

--- RabbitMeng/js/UI/ClanDetailUI.js
+++ RabbitMeng/js/UI/ClanDetailUI.js
@@ -27,8 +27,10 @@ export default class ClanDetailUI extends Component {
         super(props);
         this.state = {
             isLoading: true,
-            troops: [],
-            heroes: [],
+            troops_home: [],
+            troops_build: [],
+            heroes_home: [],
+            heroes_build: [],
             spells: [],
         };
     }
@@ -204,7 +206,20 @@ export default class ClanDetailUI extends Component {
                                 <FlatList
                                     showsHorizontalScrollIndicator={false}
                                     showsVerticalScrollIndicator={false}
-                                    data={this.state.troops}
+                                    data={this.state.troops_home}
+                                    keyExtractor={(item, index) => index}
+                                    renderItem={(item) => {
+                                        return ItemTroops.Troops(item)
+                                    }}
+                                    numColumns={6}
+                                />
+
+
+                                <Text style={{color: 'black', fontSize: 20}}>Builder Base</Text>
+                                <FlatList
+                                    showsHorizontalScrollIndicator={false}
+                                    showsVerticalScrollIndicator={false}
+                                    data={this.state.troops_build}
                                     keyExtractor={(item, index) => index}
                                     renderItem={(item) => {
                                         return ItemTroops.Troops(item)
@@ -241,7 +256,19 @@ export default class ClanDetailUI extends Component {
                                 <FlatList
                                     showsHorizontalScrollIndicator={false}
                                     showsVerticalScrollIndicator={false}
-                                    data={this.state.heroes}
+                                    data={this.state.heroes_home}
+                                    keyExtractor={(item, index) => item.name}
+                                    renderItem={(item) => {
+                                        return ItemTroops.Troops(item)
+                                    }}
+                                    numColumns={3}
+                                />
+
+                                <Text style={{color: 'black', fontSize: 20}}>Hero</Text>
+                                <FlatList
+                                    showsHorizontalScrollIndicator={false}
+                                    showsVerticalScrollIndicator={false}
+                                    data={this.state.heroes_build}
                                     keyExtractor={(item, index) => item.name}
                                     renderItem={(item) => {
                                         return ItemTroops.Troops(item)
@@ -261,16 +288,156 @@ export default class ClanDetailUI extends Component {
     componentDidMount() {
         let self = this
         HttpUtil.get('https://api.clashofclans.com/v1/players/' + self.props.coc_tag.replace(/#/, '%23'), '', function (jsonData) {
-            mainData = jsonData
-            self.setState({
-                isLoading: false,
-                troops: jsonData.troops,
-                heroes: jsonData.heroes,
-                spells: jsonData.heroes,
-            }).call(ClanDetailUI)
+            mainData = jsonData;
+            self._handle_data(self);
         })
     }
+
+    _handle_data = (self) => {
+        let troop_h = [];
+        let troop_b = [];
+        let heroes_h = [];
+        let heroes_b = [];
+
+        for (let item of mainData.troops) {
+            item.troop_img = self._setTroopsImgUrl(item.name);
+            if (item.village === 'home') {//家乡
+                troop_h.push(item)
+            } else {//建筑大师基地
+                troop_b.push(item)
+            }
+        }
+
+        for (let item of mainData.heroes) {
+            item.troop_img = self._setHeroImg(item.name);
+            if (item.village === 'home') {//家乡
+                heroes_h.push(item)
+            } else {//建筑大师基地
+                heroes_b.push(item)
+            }
+        }
+        for (let item of mainData.spells) {
+            item.troop_img = self._setSpellImg(item.name);
+        }
+
+
+        self.setState({
+            isLoading: false,
+            troops_home: troop_h,
+            troops_build: troop_b,
+            heroes_home: heroes_h,
+            heroes_build: heroes_b,
+            spells: mainData.spells,
+        }).call(ClanDetailUI)
+    }
+
+    _setTroopsImgUrl = (name) => {
+        if (name === 'Barbarian') {
+            return require('../../res/troops/barbarian.png');
+        } else if (name === 'Archer') {
+            return require('../../res/troops/archer.png');
+        } else if (name === 'Goblin') {
+            return require('../../res/troops/goblin.png');
+        } else if (name === 'Giant') {
+            return require('../../res/troops/giant.png');
+        } else if (name === 'Wall Breaker') {
+            return require('../../res/troops/wall_breaker.png');
+        } else if (name === 'Balloon') {
+            return require('../../res/troops/balloon.png');
+        } else if (name === 'Wizard') {
+            return require('../../res/troops/wizard.png');
+        } else if (name === 'Healer') {
+            return require('../../res/troops/healer.png');
+        } else if (name === 'Dragon') {
+            return require('../../res/troops/dragon.png');
+        } else if (name === 'P.E.K.K.A') {
+            return require('../../res/troops/P.E.K.K.A.png');
+        } else if (name === 'Minion') {
+            return require('../../res/troops/minion.png');
+        } else if (name === 'Hog Rider') {
+            return require('../../res/troops/hog_rider.png');
+        } else if (name === 'Valkyrie') {
+            return require('../../res/troops/valkyrie.png');
+        } else if (name === 'Golem') {
+            return require('../../res/troops/golem.png');
+        } else if (name === 'Witch') {
+            return require('../../res/troops/witch.png');
+        } else if (name === 'Lava Hound') {
+            return require('../../res/troops/lava_hound.png');
+        } else if (name === 'Bowler') {
+            return require('../../res/troops/bowler.png');
+        } else if (name === 'Baby Dragon') {
+            return require('../../res/troops/baby_dragon.png');
+        } else if (name === 'Miner') {
+            return require('../../res/troops/miner.png');
+        } else if (name === 'Raged Barbarian') {
+            return require('../../res/troops/raged_barbarian.png');
+        } else if (name === 'Sneaky Archer') {
+            return require('../../res/troops/sneaky_archer.png');
+        } else if (name === 'Beta Minion') {
+            return require('../../res/troops/beta_minion.png');
+        } else if (name === 'Boxer Giant') {
+            return require('../../res/troops/boxer_giant.png');
+        } else if (name === 'Bomber') {
+            return require('../../res/troops/bomber.png');
+        } else if (name === 'Super P.E.K.K.A') {
+            return require('../../res/troops/super_P.E.K.K.A.png');
+        } else if (name === 'Cannon Cart') {
+            return require('../../res/troops/cannon_cart.png');
+        } else if (name === 'Drop Ship') {
+            return require('../../res/troops/drop_ship.png');
+        } else if (name === 'Night Witch') {
+            return require('../../res/troops/night_witch.png');
+        } else if (name === 'Wall Wrecker') {
+            return require('../../res/imgs/question_mark.png');
+        } else if (name === 'Electro Dragon') {
+            return require('../../res/troops/electro_dragon.png');
+        } else {
+            return null;
+        }
+    }
+
+    _setSpellImg = (name) => {
+        if (name === 'Lightning Spell') {
+            return require('../../res/spells/lightning_spell.png');
+        } else if (name === 'Healing Spell') {
+            return require('../../res/spells/healing_spell.png');
+        } else if (name === 'Rage Spell') {
+            return require('../../res/spells/rage_spell.png');
+        } else if (name === 'Jump Spell') {
+            return require('../../res/spells/jump_spell.png');
+        } else if (name === 'Freeze Spell') {
+            return require('../../res/spells/freeze_spell.png');
+        } else if (name === 'Poison Spell') {
+            return require('../../res/spells/poison_spell.png');
+        } else if (name === 'Earthquake Spell') {
+            return require('../../res/spells/earthquake_spell.png');
+        } else if (name === 'Haste Spell') {
+            return require('../../res/spells/haste_spell.png');
+        } else if (name === 'Clone Spell') {
+            return require('../../res/spells/clone_spell.png');
+        } else if (name === 'Skeleton Spell') {
+            return require('../../res/spells/skeleton_spell.png');
+        } else {
+            return null;
+        }
+    }
+
+    _setHeroImg = (name) => {
+        if (name === 'Barbarian King') {
+            return require('../../res/troops/barbarian_king.png');
+        } else if (name === 'Archer Queen') {
+            return require('../../res/troops/archer_queen.png');
+        } else if (name === 'Grand Warden') {
+            return require('../../res/troops/grand_warden.png');
+        } else if (name === 'Battle Machine') {
+            return require('../../res/troops/battle_machine.png');
+        } else {
+            return null;
+        }
+    }
 }
+
 
 const styles = StyleSheet.create({
     container: {
--- RabbitMeng/js/UI/ItemTroops.js
+++ RabbitMeng/js/UI/ItemTroops.js
@@ -11,40 +11,69 @@ import {
 
 let ScreenUtil = require('../uitl/ScreenUtil');
 
-export function Troops(itemData,self) {
+export function Troops(itemData) {
+    let style_img = styles.img_normal;
+    let style_txt = styles.text_normal;
+
+    if (itemData.item.level===itemData.item.maxLevel) {
+        style_img = styles.img_max;
+        style_txt = styles.text_max;
+    }
 
     return (
         <View style={styles.container}>
-            <Image style={styles.img} source={require('../../res/troops/lava_hound.png')}/>
-            <Text style={styles.text}>{itemData.item.level}</Text>
+            <Image style={style_img} source={itemData.item.troop_img}/>
+            <Text style={style_txt}>{itemData.item.level}</Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
+        flex: 1,
+        width: ScreenUtil.scaleSize(50),
+        height: ScreenUtil.scaleSize(50),
+        margin: ScreenUtil.scaleSize(1),
+
+    },
+    img_normal: {
+        borderWidth: ScreenUtil.scaleSize(1),
+        borderColor: '#77769F',
+        borderRadius: ScreenUtil.scaleSize(5),
         width: ScreenUtil.scaleSize(50),
         height: ScreenUtil.scaleSize(50),
     },
-    img: {
-        margin:ScreenUtil.scaleSize(1),
-        borderWidth:ScreenUtil.scaleSize(1),
-        borderColor:'blue',
-        borderRadius:ScreenUtil.scaleSize(5),
+    img_max: {
+        borderWidth: ScreenUtil.scaleSize(1),
+        borderColor: '#F8C500',
+        borderRadius: ScreenUtil.scaleSize(5),
         width: ScreenUtil.scaleSize(50),
         height: ScreenUtil.scaleSize(50),
     },
-    text: {
-        textAlign:'center',
-        borderRadius:ScreenUtil.scaleSize(5),
-        position:'absolute',
-        marginLeft:ScreenUtil.scaleSize(2),
-        marginBottom:ScreenUtil.scaleSize(2),
-        fontSize:7,
-        marginTop:ScreenUtil.scaleSize(35),
-        width: ScreenUtil.scaleSize(14),
-        height: ScreenUtil.scaleSize(14),
-        backgroundColor: 'yellow',
+    text_normal: {
+        paddingRight:ScreenUtil.scaleSize(2),
+        textAlign: 'center',
+        position: 'absolute',
+        marginLeft: ScreenUtil.scaleSize(1),
+        marginBottom: ScreenUtil.scaleSize(1),
+        fontSize: 8,
+        color:'black',
+        marginTop: ScreenUtil.scaleSize(27),
+        backgroundColor: 'rgba(255,255,255,0.8)',
+        borderBottomLeftRadius:ScreenUtil.scaleSize(5),
+        borderTopRightRadius:ScreenUtil.scaleSize(3),
+    },
+    text_max: {
+        borderBottomLeftRadius:ScreenUtil.scaleSize(5),
+        borderTopRightRadius:ScreenUtil.scaleSize(3),
+        paddingRight:ScreenUtil.scaleSize(2),
+        textAlign: 'center',
+        position: 'absolute',
+        marginLeft: ScreenUtil.scaleSize(1),
+        marginBottom: ScreenUtil.scaleSize(1),
+        fontSize: 8,
+        color:'white',
+        marginTop: ScreenUtil.scaleSize(27),
+        backgroundColor: '#F8C500',
     }
 });
