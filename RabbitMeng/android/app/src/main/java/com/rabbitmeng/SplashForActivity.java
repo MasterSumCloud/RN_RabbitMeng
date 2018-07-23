62f6bc4eb48068984b3a61dbcd81d22c3bafd62e
Mac_py <summer.p.y@foxmail.com>
2018-07-23 18:00:56 +0800

    安卓启动页制作

---
+++ RabbitMeng/android/app/src/main/java/com/rabbitmeng/SplashForActivity.java
@@ -0,0 +1,33 @@
+package com.rabbitmeng;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Master on 23/07/2018.
+ */
+
+public class SplashForActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_act);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashForActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
